fe33a1e65a9ac644a58f5344193052121aac63f3
kohei <kohei3400@gmail.com>
2022-06-19 12:23:55 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import {React,useState} from 'react';
+import Title from './Title'
+import TaskList from './TaskList'
+import InputForm from './InputForm'
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <TaskList />
+      <InputForm />
     </div>
   );
 }
